230eee0b43fc24044bdc909cfa7c82cf7126ddd6
Rituraj SIngh <113875316+irituraj@users.noreply.github.com>
2022-09-27 23:44:33 +0530

    Add files via upload

---
+++ Comparison.java
@@ -0,0 +1,15 @@
+package assignment;
+
+public class Comparison {
+
+	public static void main(String[] args) {
+		int num1 = 56;
+		int num2 = 7;
+		int num3 = 18;
+		
+		if(num1>=20 && (num1<num2  && num1<num3))
+			System.out.println("Condition is True");
+		else
+			System.out.println("Condition is False");
+	}
+}
